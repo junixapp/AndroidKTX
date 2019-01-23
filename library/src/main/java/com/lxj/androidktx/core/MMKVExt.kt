e4126b9240e92347ff7ebb861c81aacce1cadff3
li-xiaojun <16167479@qq.com>
2019-01-23 10:13:37 +0800

    MMKVExt增加addToSet方法

--- library/src/main/java/com/lxj/androidktx/core/MMKVExt.kt
+++ library/src/main/java/com/lxj/androidktx/core/MMKVExt.kt
@@ -8,3 +8,15 @@ import com.tencent.mmkv.MMKV
  */
 
 fun Any.mmkv(id: String? = null) = if(id==null)MMKV.defaultMMKV() else MMKV.mmkvWithID(id)
+
+/**
+ * 将一个字符串添加到Set中
+ * @param key key值
+ * @param s 要添加的字符串
+ */
+fun MMKV.addToSet(key: String, s: String){
+    getStringSet(key, mutableSetOf())?.apply {
+        add(s)
+        putStringSet(key, this)
+    }
+}
