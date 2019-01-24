d3c6a71a882724ada44b9c46ba96a84a7913c94d
li-xiaojun <16167479@qq.com>
2019-01-24 14:23:49 +0800

    change SpanExt target to CharSequence

--- library/src/main/java/com/lxj/androidktx/core/SpanExt.kt
+++ library/src/main/java/com/lxj/androidktx/core/SpanExt.kt
@@ -20,7 +20,7 @@ import android.widget.TextView
  * @param range 要改变大小的文字的范围
  * @param scale 缩放值，大于1，则比其他文字大；小于1，则比其他文字小；默认是1.5
  */
-fun String.toSizeSpan(range: IntRange, scale: Float = 1.5f): SpannableString {
+fun CharSequence.toSizeSpan(range: IntRange, scale: Float = 1.5f): SpannableString {
     return SpannableString(this).apply {
         setSpan(RelativeSizeSpan(scale), range.start, range.endInclusive, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
     }
@@ -31,7 +31,7 @@ fun String.toSizeSpan(range: IntRange, scale: Float = 1.5f): SpannableString {
  * @param range 要改变前景色的文字的范围
  * @param color 要改变的颜色，默认是红色
  */
-fun String.toColorSpan(range: IntRange, color: Int = Color.RED): SpannableString {
+fun CharSequence.toColorSpan(range: IntRange, color: Int = Color.RED): SpannableString {
     return SpannableString(this).apply {
         setSpan(ForegroundColorSpan(color), range.start, range.endInclusive, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
     }
@@ -42,7 +42,7 @@ fun String.toColorSpan(range: IntRange, color: Int = Color.RED): SpannableString
  * @param range 要改变背景色的文字的范围
  * @param color 要改变的颜色，默认是红色
  */
-fun String.toBackgroundColorSpan(range: IntRange, color: Int = Color.RED): SpannableString {
+fun CharSequence.toBackgroundColorSpan(range: IntRange, color: Int = Color.RED): SpannableString {
     return SpannableString(this).apply {
         setSpan(BackgroundColorSpan(color), range.start, range.endInclusive, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
     }
@@ -52,7 +52,7 @@ fun String.toBackgroundColorSpan(range: IntRange, color: Int = Color.RED): Spann
  * 将一段文字中指定range的文字添加删除线
  * @param range 要添加删除线的文字的范围
  */
-fun String.toStrikeThrougthSpan(range: IntRange): SpannableString {
+fun CharSequence.toStrikeThrougthSpan(range: IntRange): SpannableString {
     return SpannableString(this).apply {
         setSpan(StrikethroughSpan(), range.start, range.endInclusive, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
     }
@@ -62,7 +62,7 @@ fun String.toStrikeThrougthSpan(range: IntRange): SpannableString {
  * 将一段文字中指定range的文字添加颜色和点击事件
  * @param range 目标文字的范围
  */
-fun String.toClickSpan(range: IntRange, color: Int = Color.RED, isUnderlineText: Boolean = false,clickListener: View.OnClickListener): SpannableString {
+fun CharSequence.toClickSpan(range: IntRange, color: Int = Color.RED, isUnderlineText: Boolean = false,clickListener: View.OnClickListener): SpannableString {
     return SpannableString(this).apply {
         val clickableSpan = object : ClickableSpan(){
             override fun onClick(widget: View) {
