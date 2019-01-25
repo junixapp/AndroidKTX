05e3610751e5faa539312c3eccaceb2a18925d0e
li-xiaojun <16167479@qq.com>
2019-01-25 09:08:02 +0800

    修复加载图片时CenterCrop导致圆角失效的问题

--- library/src/main/java/com/lxj/androidktx/core/ImageViewExt.kt
+++ library/src/main/java/com/lxj/androidktx/core/ImageViewExt.kt
@@ -2,6 +2,7 @@ package com.lxj.androidktx.core
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
@@ -16,22 +17,23 @@ import com.bumptech.glide.request.RequestOptions
  * @param url 可以是网络，可以是File，可以是资源id等等Glide支持的类型
  * @param placeholder 默认占位图
  * @param error 失败占位图
- * @param isCenterCrop 默认为true
  * @param isCircle 是否是圆形，默认false，注意：isCircle和roundRadius两个只能有一个生效
  * @param roundRadius 圆角角度，默认为0，不带圆角，注意：isCircle和roundRadius两个只能有一个生效
  * @param isCrossFade 是否有过渡动画，默认没有过渡动画
  */
 fun ImageView.load(url: Any, placeholder: Int = 0, error: Int = 0,
-                   isCenterCrop: Boolean = true,
                    isCircle: Boolean = false,
                    roundRadius: Int = 0,
                    isCrossFade: Boolean = false) {
     val options = RequestOptions().placeholder(placeholder).error(error).apply {
-        if (isCenterCrop) centerCrop()
         if (isCircle) {
             circleCrop()
         } else if (roundRadius != 0) {
-            transform(RoundedCorners(roundRadius))
+            if (scaleType == ImageView.ScaleType.CENTER_CROP) {
+                transforms(CenterCrop(), RoundedCorners(roundRadius))
+            } else {
+                transform(RoundedCorners(roundRadius))
+            }
         }
     }
     Glide.with(context).load(url)
