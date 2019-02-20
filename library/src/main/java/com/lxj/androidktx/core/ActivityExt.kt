66706d5e8b6471949638b538eea50bc3d4ef9af6
li-xiaojun <16167479@qq.com>
2019-02-20 14:34:20 +0800

    add finishDelay()

--- library/src/main/java/com/lxj/androidktx/core/ActivityExt.kt
+++ library/src/main/java/com/lxj/androidktx/core/ActivityExt.kt
@@ -3,6 +3,8 @@ package com.lxj.androidktx.core
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.support.v4.app.Fragment
 import android.view.View
 
@@ -11,40 +13,44 @@ import android.view.View
  * Create by lxj, at 2018/12/7
  */
 
-inline fun <reified T> Fragment.startActivity(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null){
+inline fun <reified T> Fragment.startActivity(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null) {
     activity?.startActivity<T>(flag, bundle)
 }
 
-inline fun <reified T> Fragment.startActivityForResult(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null, requestCode: Int = -1){
+inline fun <reified T> Fragment.startActivityForResult(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null, requestCode: Int = -1) {
     activity?.startActivityForResult<T>(flag, bundle, requestCode)
 }
 
-inline fun <reified T> Context.startActivity(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null, requestCode: Int = -1){
+inline fun <reified T> Context.startActivity(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null, requestCode: Int = -1) {
     val intent = Intent(this, T::class.java).apply {
-        if(flag!=-1) {
+        if (flag != -1) {
             this.addFlags(flag)
-        }else if(this !is Activity){
+        } else if (this !is Activity) {
             this.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
-        if (bundle!=null)putExtras(bundle.toBundle())
+        if (bundle != null) putExtras(bundle.toBundle())
     }
     startActivity(intent)
 }
 
-inline fun <reified T> View.startActivity(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null){
+inline fun <reified T> View.startActivity(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null) {
     context.startActivity<T>(flag, bundle)
 }
 
-inline fun <reified T> View.startActivityForResult(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null, requestCode: Int = -1){
+inline fun <reified T> View.startActivityForResult(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null, requestCode: Int = -1) {
     (context as Activity).startActivityForResult<T>(flag, bundle, requestCode)
 }
 
-inline fun <reified T> Activity.startActivityForResult(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null, requestCode: Int = -1){
+inline fun <reified T> Activity.startActivityForResult(flag: Int = -1, bundle: Array<out Pair<String, Any?>>? = null, requestCode: Int = -1) {
     val intent = Intent(this, T::class.java).apply {
-        if(flag!=-1) {
+        if (flag != -1) {
             this.addFlags(flag)
         }
-        if (bundle!=null)putExtras(bundle.toBundle())
+        if (bundle != null) putExtras(bundle.toBundle())
     }
     startActivityForResult(intent, requestCode)
+}
+
+fun Activity.finishDelay(delay: Long = 1) {
+    Handler(Looper.getMainLooper()).postDelayed({ finish() }, delay)
 }
