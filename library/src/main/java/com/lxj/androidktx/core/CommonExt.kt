32f5f7c0378d1a1b2d145e654bfe1b329d2328da
li-xiaojun <16167479@qq.com>
2019-05-25 14:58:41 +0800

    发布1.1.20-x

--- library/src/main/java/com/lxj/androidktx/core/CommonExt.kt
+++ library/src/main/java/com/lxj/androidktx/core/CommonExt.kt
@@ -10,17 +10,15 @@ import android.graphics.drawable.RippleDrawable
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
-import android.support.v4.app.Fragment
-import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import com.lxj.androidktx.AndroidKtxConfig
 import com.lxj.androidktx.util.NetworkUtils
 import java.io.Serializable
-import java.lang.reflect.Type
 
 /**
  * Description:  通用扩展
