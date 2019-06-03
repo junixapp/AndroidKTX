9f19ff32f823203baee9a9f9af5c81cde4719273
li-xiaojun <16167479@qq.com>
2019-06-03 21:16:06 +0800

    发布1.1.26

--- library/src/main/java/com/lxj/androidktx/core/ImageViewExt.kt
+++ library/src/main/java/com/lxj/androidktx/core/ImageViewExt.kt
@@ -25,7 +25,7 @@ import com.bumptech.glide.request.target.Target
  * @param isCrossFade 是否有过渡动画，默认没有过渡动画
  * @param isForceOriginalSize 是否强制使用原图，默认false
  */
-fun ImageView.load(url: Any, placeholder: Int = 0, error: Int = 0,
+fun ImageView.load(url: Any?, placeholder: Int = 0, error: Int = 0,
                    isCircle: Boolean = false,
                    isCenterCrop: Boolean = false,
                    roundRadius: Int = 0,
