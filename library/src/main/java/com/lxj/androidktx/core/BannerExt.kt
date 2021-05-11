be6a924a97af7f29dafd028189eed3adcda3ed49
lixiaojun <16167479@qq.com>
2021-05-11 16:18:15 +0800

    增加CommonBannerAdapter

---
+++ library/src/main/java/com/lxj/androidktx/core/BannerExt.kt
@@ -0,0 +1,35 @@
+package com.lxj.androidktx.core
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.lxj.androidktx.R
+import com.youth.banner.adapter.BannerAdapter
+
+class CommonBannerAdapter(data: List<Any>, var margin: Int = 0, var cornerRadius: Int = 0,
+    var pageElevation: Int = 0, var onItemClick: ((Int)->Unit)? = null) :
+    BannerAdapter<Any, CommonBannerAdapter.BannerViewHolder>(data) {
+    override fun onCreateHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
+        val item = LayoutInflater.from(parent.context).inflate(R.layout._ktx_adapter_common_banner, parent, false)
+        return BannerViewHolder(item)
+    }
+
+    override fun onBindView(holder: BannerViewHolder, t: Any, position: Int, size: Int) {
+        holder.itemView.margin(margin, margin, margin, margin)
+        holder.imageView.load(t, isCrossFade = true)
+        (holder.itemView as CardView).apply {
+            radius = cornerRadius.toFloat()
+            cardElevation = pageElevation.toFloat()
+            click { onItemClick?.invoke(position) }
+        }
+    }
+
+    inner class BannerViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        var imageView = itemView.findViewById<ImageView>(R.id.image)
+    }
+}
+
