b7b6565cdf692da9169581c7796abe15f814bdef
lixiaojun <16167479@qq.com>
2021-05-23 22:28:59 +0800

    增加convertToDeffered方法，将任意代码块转为CompletableDeffered

---
+++ library/src/main/java/com/lxj/androidktx/core/CoroutineExt.kt
@@ -0,0 +1,13 @@
+package com.lxj.androidktx.core
+
+import kotlinx.coroutines.CompletableDeferred
+
+/**
+ * 将任意代码块转为CompletableDeferred，你需要自己调用deffered.complete(T)
+ */
+fun <T> convertToDeffered(action: (deffered: CompletableDeferred<T>)->Unit): CompletableDeferred<T> {
+    val deferred = CompletableDeferred<T>()
+    deferred.invokeOnCompletion {}
+    action(deferred)
+    return deferred
+}
