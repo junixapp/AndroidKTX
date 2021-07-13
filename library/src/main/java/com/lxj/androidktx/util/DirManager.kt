fec663b138025d3ae3d269bb31e17cd0ba2eaae3
lixiaojun <16167479@qq.com>
2021-07-13 22:44:27 +0800

    完善DirManager

--- library/src/main/java/com/lxj/androidktx/util/DirManager.kt
+++ library/src/main/java/com/lxj/androidktx/util/DirManager.kt
@@ -12,16 +12,17 @@ object DirManager{
     val tempDir: String by lazy { "${rootDir}/temp" }
     val downloadDir: String by lazy { "${rootDir}/download" }
     val cacheDir: String by lazy { "${rootDir}/cache" }
+    //可以共享给三方的文件目录
+    val shareDir: String by lazy { "${rootDir}/share" }
 
     fun init(){
-        val sdcardExist = Environment.MEDIA_MOUNTED == Environment.getExternalStorageState() || !Environment.isExternalStorageRemovable()
-        rootDir = if(sdcardExist) context.getExternalFilesDir(null)?.absolutePath?:""
-        else context.filesDir.absolutePath
+        rootDir = context.getExternalFilesDir(null)?.absolutePath?: context.filesDir.absolutePath
 
         createDirIfNotExist(rootDir)
         createDirIfNotExist(tempDir)
         createDirIfNotExist(downloadDir)
         createDirIfNotExist(cacheDir)
+        createDirIfNotExist(shareDir)
     }
 
     private fun createDirIfNotExist(path: String){
