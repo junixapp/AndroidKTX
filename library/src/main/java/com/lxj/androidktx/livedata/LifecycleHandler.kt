1dc43b8563a5bfa3d7922058ecbe2c1e973cdcdd
lixiaojun <16167479@qq.com>
2021-08-24 18:22:45 +0800

    优化LifecycleHandler

--- library/src/main/java/com/lxj/androidktx/livedata/LifecycleHandler.kt
+++ library/src/main/java/com/lxj/androidktx/livedata/LifecycleHandler.kt
@@ -6,16 +6,16 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
+import com.blankj.utilcode.util.ThreadUtils
 
 /**
  * Description: 自动在UI销毁时移除msg和任务的Handler，不会有内存泄露
  * Create by dance, at 2019/5/21
  */
-open class LifecycleHandler(private val lifecycleOwner: LifecycleOwner?,
-                       looper: Looper = Looper.getMainLooper())
-    : Handler(looper), LifecycleObserver {
+open class LifecycleHandler(private val lifecycleOwner: LifecycleOwner?)
+    : Handler(Looper.getMainLooper()), LifecycleObserver {
     init {
-        lifecycleOwner?.lifecycle?.addObserver(this)
+        ThreadUtils.runOnUiThread { lifecycleOwner?.lifecycle?.addObserver(this) }
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
