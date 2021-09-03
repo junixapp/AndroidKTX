fa045566105b0a8c75b8a670db81106e810ebc6c
lixiaojun <16167479@qq.com>
2021-09-03 20:52:26 +0800

    优化TokenInterceptor

--- library/src/main/java/com/lxj/androidktx/okhttp/TokenInterceptor.kt
+++ library/src/main/java/com/lxj/androidktx/okhttp/TokenInterceptor.kt
@@ -1,5 +1,7 @@
 package com.lxj.androidktx.okhttp
 
+import com.blankj.utilcode.util.NetworkUtils
+import com.blankj.utilcode.util.ToastUtils
 import com.lxj.androidktx.core.sp
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -7,11 +9,20 @@ import java.nio.charset.Charset
 
 /**
  * 通用的Token拦截器，支持json response text callback.
+ * @param tokenField token的字段名
+ * @param tokenCreator token值的获取函数
+ * @param networkErrorToast 网络失败的提示文字，如果为空则不检测网络失败
+ * @param onGetBodyData 获取到响应数据的回调
  */
 class TokenInterceptor(var tokenField: String = "token",
                        var tokenCreator: (()->String)? = null,
+                       var networkErrorToast: String? = null,
                        var onGetBodyData: (url: String,json: String) -> Unit  ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        if(!networkErrorToast.isNullOrEmpty() && !NetworkUtils.isConnected()){
+            ToastUtils.showShort(networkErrorToast)
+        }
+
         var request = chain.request()
         val tokenValue = if(tokenCreator!=null) tokenCreator!!() else sp().getString("token", null)
         if (tokenValue != null) {
