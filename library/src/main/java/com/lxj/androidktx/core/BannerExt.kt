8207bfe451087e194f786cdcee7871d0b02d25f1
lixiaojun <16167479@qq.com>
2021-09-08 11:30:49 +0800

    增强CommonBannerAdapter

--- library/src/main/java/com/lxj/androidktx/core/BannerExt.kt
+++ library/src/main/java/com/lxj/androidktx/core/BannerExt.kt
@@ -1,5 +1,6 @@
 package com.lxj.androidktx.core
 
+import android.graphics.Rect
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,8 +10,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.lxj.androidktx.R
 import com.youth.banner.adapter.BannerAdapter
 
-class CommonBannerAdapter(data: List<Any>, var margin: Int = 0, var cornerRadius: Int = 0,
-    var pageElevation: Int = 0, var onItemClick: ((Int)->Unit)? = null) :
+class CommonBannerAdapter(data: List<Any>, var margin: Rect = Rect(), var cornerRadius: Int = 0,
+                          var pageElevation: Int = 0, var onItemClick: ((Int)->Unit)? = null) :
     BannerAdapter<Any, CommonBannerAdapter.BannerViewHolder>(data) {
     override fun onCreateHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
         val item = LayoutInflater.from(parent.context).inflate(R.layout._ktx_adapter_common_banner, parent, false)
@@ -18,7 +19,7 @@ class CommonBannerAdapter(data: List<Any>, var margin: Int = 0, var cornerRadius
     }
 
     override fun onBindView(holder: BannerViewHolder, t: Any, position: Int, size: Int) {
-        holder.itemView.margin(margin, margin, margin, margin)
+        holder.itemView.margin(margin.left, margin.top, margin.right, margin.bottom)
         holder.imageView.load(t, isCrossFade = true)
         (holder.itemView as CardView).apply {
             radius = cornerRadius.toFloat()
