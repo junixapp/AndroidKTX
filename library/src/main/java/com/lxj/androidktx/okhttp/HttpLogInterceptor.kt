2cc4a63a84547ac3caddfd3dc7b23e71b47d5775
lixiaojun <16167479@qq.com>
2021-11-29 15:14:59 +0800

    优化HttpLogInterceptor

--- library/src/main/java/com/lxj/androidktx/okhttp/HttpLogInterceptor.kt
+++ library/src/main/java/com/lxj/androidktx/okhttp/HttpLogInterceptor.kt
@@ -18,27 +18,31 @@ import java.util.concurrent.TimeUnit
  * Description: A better http log interceptor.
  * Create by dance, at 2019/1/2
  */
-class HttpLogInterceptor @JvmOverloads constructor(var printResponseHeader: Boolean = true) : Interceptor {
+class HttpLogInterceptor @JvmOverloads constructor(var printResponseHeader: Boolean = true) :
+    Interceptor {
     private val requestPrefixStart = "-------->"
     private val requestPrefixEnd = "--------------------------------------->"
     private val responsePrefixStart = "<--------"
     private val responsePrefixEnd = "<-------------------------------------"
     private val tag = "HttpLogInterceptor"
+
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        val requestBody = request.body()
-        val connection = chain.connection()
-        // 1. 请求第一行
-        var requestMessage = "$requestPrefixStart ${request.method()} ${request.url()} ${if (connection != null) connection.protocol() else ""}\n"
-        // 2. 请求头
-        requestMessage += header2String(request.headers())
-        // 3. 请求体
-        if (bodyHasUnknownEncoding(request.headers())) {
-            requestMessage += "\n$requestPrefixEnd END ${request.method()} (encoded body omitted)"
-        } else if (requestBody != null) {
-            requestMessage += "\n"
-            val contentType = requestBody.contentType()
+        try {
+            val request = chain.request()
+            val requestBody = request.body()
+            val connection = chain.connection()
+            // 1. 请求第一行
+            var requestMessage =
+                "$requestPrefixStart ${request.method()} ${request.url()} ${if (connection != null) connection.protocol() else ""}\n"
+            // 2. 请求头
+            requestMessage += header2String(request.headers())
+            // 3. 请求体
+            if (bodyHasUnknownEncoding(request.headers())) {
+                requestMessage += "\n$requestPrefixEnd END ${request.method()} (encoded body omitted)"
+            } else if (requestBody != null) {
+                requestMessage += "\n"
+                val contentType = requestBody.contentType()
 //            if(contentType.toString().contains("multipart")){
 //                requestMessage += "\n$requestPrefixEnd END ${request.method()} (multipart binary ${requestBody.contentLength()} -byte body omitted)"
 //            } else{
@@ -56,83 +60,89 @@ class HttpLogInterceptor @JvmOverloads constructor(var printResponseHeader: Bool
                     requestMessage += "\n$requestPrefixEnd END ${request.method()} (binary ${requestBody.contentLength()} -byte body omitted)"
                 }
 //            }
-        } else {
-            requestMessage += "\n$requestPrefixEnd END ${request.method()} (no request body)"
-        }
-        // 4. 打印请求信息
-        log(requestMessage)
+            } else {
+                requestMessage += "\n$requestPrefixEnd END ${request.method()} (no request body)"
+            }
+            // 4. 打印请求信息
+            log(requestMessage)
 
-        val startNs = System.nanoTime()
-        val response: Response
-        try {
-            response = chain.proceed(request)
-        } catch (e: Exception) {
-            log("$responsePrefixStart HTTP FAILED: $e")
+            val startNs = System.nanoTime()
+            val response: Response
+            try {
+                response = chain.proceed(request)
+            } catch (e: Exception) {
+                log("$responsePrefixStart HTTP FAILED: $e")
+                throw e
+            }
+
+            val tookMs = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs)
+            val responseBody = response.body()
+            val contentLength = responseBody!!.contentLength()
+            val bodySize = if (contentLength != -1L) "$contentLength-byte" else "unknown-length"
+            var responseMessage = "$responsePrefixStart ${response.code()} ${request.method()} ${
+                if (response.message().isEmpty()) "" else response.message()
+            } "
+            responseMessage += response.request().url()
+            responseMessage += " (" + tookMs + "ms" + ", $bodySize body)\n"
+
+            // 是否拼接打印头
+            val headers = response.headers()
+            if (printResponseHeader) {
+                responseMessage += header2String(headers)
+            }
+
+            if (!HttpHeaders.hasBody(response)) {
+                responseMessage += "\n$responsePrefixEnd END HTTP"
+            } else if (bodyHasUnknownEncoding(response.headers())) {
+                responseMessage += "\n$responsePrefixEnd END HTTP (encoded body omitted)"
+            } else {
+                val source = responseBody.source()
+                source.request(java.lang.Long.MAX_VALUE) // Buffer the entire body.
+                var buffer = source.buffer()
+
+                var gzippedLength: Long? = null
+                if ("gzip".equals(headers.get("Content-Encoding"), ignoreCase = true)) {
+                    gzippedLength = buffer.size()
+                    GzipSource(buffer.clone()).use { gzippedResponseBody ->
+                        buffer = Buffer()
+                        buffer.writeAll(gzippedResponseBody)
+                    }
+                }
+
+                var charset: Charset? = UTF8
+                val contentType = responseBody.contentType()
+                if (contentType != null) {
+                    charset = contentType.charset(UTF8)
+                }
+
+                responseMessage += "\n"
+                if (!isPlaintext(buffer)) {
+                    responseMessage += "\n$responsePrefixEnd END HTTP (binary " + buffer.size() + "-byte body omitted)"
+                    return response
+                }
+
+                if (contentLength != 0L) {
+                    val responseData = buffer.clone().readString(charset!!)
+                    responseMessage += try {
+                        JSONObject(responseData).toString(2)
+                    } catch (e: JSONException) {
+                        // 不是json
+                        responseData
+                    }
+                }
+
+                responseMessage += if (gzippedLength != null) {
+                    "\n$responsePrefixEnd END HTTP (" + buffer.size() + "-byte, $gzippedLength-gzipped-byte body)"
+                } else {
+                    "\n$responsePrefixEnd END HTTP (" + buffer.size() + "-byte body)"
+                }
+            }
+            log(responseMessage)
+            return response
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
             throw e
         }
-
-        val tookMs = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs)
-        val responseBody = response.body()
-        val contentLength = responseBody!!.contentLength()
-        val bodySize = if (contentLength != -1L) "$contentLength-byte" else "unknown-length"
-        var responseMessage = "$responsePrefixStart ${response.code()} ${request.method()} ${if (response.message().isEmpty()) "" else response.message()} "
-        responseMessage += response.request().url()
-        responseMessage += " (" + tookMs + "ms" + ", $bodySize body)\n"
-
-        // 是否拼接打印头
-        val headers = response.headers()
-        if (printResponseHeader){
-            responseMessage += header2String(headers)
-        }
-
-        if (!HttpHeaders.hasBody(response)) {
-            responseMessage += "\n$responsePrefixEnd END HTTP"
-        } else if (bodyHasUnknownEncoding(response.headers())) {
-            responseMessage += "\n$responsePrefixEnd END HTTP (encoded body omitted)"
-        } else {
-            val source = responseBody.source()
-            source.request(java.lang.Long.MAX_VALUE) // Buffer the entire body.
-            var buffer = source.buffer()
-
-            var gzippedLength: Long? = null
-            if ("gzip".equals(headers.get("Content-Encoding"), ignoreCase = true)) {
-                gzippedLength = buffer.size()
-                GzipSource(buffer.clone()).use { gzippedResponseBody ->
-                    buffer = Buffer()
-                    buffer.writeAll(gzippedResponseBody)
-                }
-            }
-
-            var charset: Charset? = UTF8
-            val contentType = responseBody.contentType()
-            if (contentType != null) {
-                charset = contentType.charset(UTF8)
-            }
-
-            responseMessage += "\n"
-            if (!isPlaintext(buffer)) {
-                responseMessage += "\n$responsePrefixEnd END HTTP (binary " + buffer.size() + "-byte body omitted)"
-                return response
-            }
-
-            if (contentLength != 0L) {
-                val responseData = buffer.clone().readString(charset!!)
-                responseMessage += try {
-                    JSONObject(responseData).toString(2)
-                } catch (e: JSONException) {
-                    // 不是json
-                    responseData
-                }
-            }
-
-            responseMessage += if (gzippedLength != null) {
-                "\n$responsePrefixEnd END HTTP (" + buffer.size() + "-byte, $gzippedLength-gzipped-byte body)"
-            } else {
-                "\n$responsePrefixEnd END HTTP (" + buffer.size() + "-byte body)"
-            }
-        }
-        log(responseMessage)
-        return response
     }
 
     private fun header2String(headers: Headers): String {
@@ -190,8 +200,8 @@ class HttpLogInterceptor @JvmOverloads constructor(var printResponseHeader: Bool
                     && !contentEncoding.equals("gzip", ignoreCase = true))
         }
     }
-    
-    private fun log(msg: String){
+
+    private fun log(msg: String) {
         Log.d(tag, msg)
     }
 }
