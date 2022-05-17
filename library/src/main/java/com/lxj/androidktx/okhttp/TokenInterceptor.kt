97b133fbf13a6e205ae6d3347e84796429c42c15
16167479@qq.com <16167479@qq.com>
2022-05-17 21:19:19 +0800

    优化TokenInterceptor

--- library/src/main/java/com/lxj/androidktx/okhttp/TokenInterceptor.kt
+++ library/src/main/java/com/lxj/androidktx/okhttp/TokenInterceptor.kt
@@ -12,12 +12,12 @@ import java.nio.charset.Charset
  * @param tokenField token的字段名
  * @param tokenCreator token值的获取函数
  * @param networkErrorToast 网络失败的提示文字，如果为空则不检测网络失败
- * @param onGetBodyData 获取到响应数据的回调
+ * @param onGetJsonData 获取到响应数据的回调
  */
 class TokenInterceptor(var tokenField: String = "token",
                        var tokenCreator: (()->String)? = null,
                        var networkErrorToast: String? = null,
-                       var onGetBodyData: ((url: String,json: String) -> Unit)? = null  ) : Interceptor {
+                       var onGetJsonData: ((url: String,json: String) -> Unit)? = null  ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         if(!networkErrorToast.isNullOrEmpty() && !NetworkUtils.isConnected()){
             ToastUtils.showShort(networkErrorToast)
@@ -34,7 +34,7 @@ class TokenInterceptor(var tokenField: String = "token",
             val source = response.body()?.source()
             source?.request(java.lang.Long.MAX_VALUE) // Buffer the entire body.
             val data = source?.buffer()?.clone()?.readString(Charset.forName("UTF-8"))
-            if (data != null && onGetBodyData!=null) onGetBodyData!!(request.url().toString(), data)
+            if (data != null && onGetJsonData!=null) onGetJsonData!!(request.url().toString(), data)
         }
         return response
     }
