633d34cacc3dde0debfe4980bf8297702093f9f4
16167479@qq.com <16167479@qq.com>
2022-07-23 23:41:49 +0800

    ImagePicker优化

--- matisse/src/main/java/com/zhihu/matisse/ui/MatisseActivity.java
+++ matisse/src/main/java/com/zhihu/matisse/ui/MatisseActivity.java
@@ -147,7 +147,8 @@ public class MatisseActivity extends AppCompatActivity implements
         mAlbumsAdapter = new AlbumsAdapter(this, null, false);
         mAlbumsSpinner = new AlbumsSpinner(this);
         mAlbumsSpinner.setOnItemSelectedListener(this);
-        mAlbumsSpinner.setSelectedTextView((TextView) findViewById(R.id.selected_album));
+        TextView titleAlbum = (TextView) findViewById(R.id.selected_album);
+        mAlbumsSpinner.setSelectedTextView(titleAlbum);
         mAlbumsSpinner.setPopupAnchorView(findViewById(R.id.toolbar));
         mAlbumsSpinner.setAdapter(mAlbumsAdapter);
         mAlbumCollection.onCreate(this, this);
