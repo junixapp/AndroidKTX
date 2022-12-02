e4b2f6f7b6ade8c3b55ae3aa56c1ef359f8f1feb
jun <16167479@qq.com>
2022-12-02 21:56:37 +0800

    优化代码

--- library/src/main/java/com/lxj/androidktx/okhttp/SmartInterceptor.kt
+++ library/src/main/java/com/lxj/androidktx/okhttp/SmartInterceptor.kt
@@ -11,16 +11,20 @@ import okhttp3.Response
 import java.nio.charset.Charset
 
 /**
- * 通用的Token拦截器，支持json response text callback.
+ * 通用的拦截器，支持json response text callback.
+ * 1. 支持token插入
+ * 2. 支持response的hook，适用于要对返回数据进行解密的场景
  * @param tokenField token的字段名
  * @param tokenCreator token值的获取函数
  * @param networkErrorToast 网络失败的提示文字，如果为空则不检测网络失败
  * @param onGetJsonData 获取到响应数据的回调
  */
-class TokenInterceptor(var tokenField: String = "token",
+class SmartInterceptor(var tokenField: String = "token",
                        var tokenCreator: (()->String)? = null,
                        var networkErrorToast: String? = null,
-                       var onGetJsonData: ((url: String,json: String, header: Headers) -> Unit)? = null ) : Interceptor {
+                       var onGetJsonData: ((url: String,json: String, header: Headers) -> Unit)? = null ,
+                       var onHookResponse: ((url: String, data: String?, response: Response) -> Response)? = null
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         if(!networkErrorToast.isNullOrEmpty() && !NetworkUtils.isConnected()){
             ToastUtils.showShort(networkErrorToast)
@@ -35,12 +39,12 @@ class TokenInterceptor(var tokenField: String = "token",
         val contentType = response.header("content-type")
         if (contentType?.contains("application/json") == true ||
             contentType?.contains("text/html") == true) { //存在一些不遵守规范的后端选手
-            //json type
             val source = response.body()?.source()
             source?.request(java.lang.Long.MAX_VALUE) // Buffer the entire body.
             val data = source?.buffer()?.clone()?.readString(Charset.forName("UTF-8"))
             val isJson = data.isJsonObject() || data.isJsonArray()
-            if (isJson && onGetJsonData!=null) onGetJsonData!!(request.url().toString(), data!!, response.headers())
+            if (isJson) onGetJsonData?.invoke(request.url().toString(), data!!, response.headers())
+            if (onHookResponse!=null) return onHookResponse!!(request.url().toString(), data, response)
         }
         return response
     }
